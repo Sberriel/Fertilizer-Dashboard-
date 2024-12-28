50accda6fcc64c7cccaa5b2dfcf3b00fa3c32fc7
Sberriel <sba24281@student.cct.ie>
2024-12-28 11:30:39 +0000

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,24 +1,28 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('FAOSTAT.csv')
 
- st.title("Fertilizers Dashboard")
+df = pd.read_csv("FAOSTAT.csv")
+
+
+st.title("Fertilizers Dashboard")
 
 
 st.markdown(
     """
     <style>
     .custom-title {
-        font-size: 85px;                  /* Large font size */
-        color: #0FA3B1;                   /* Green-blue color */
+        font-size: 85px;  /* Large font size */
+        color: #0FA3B1;   /* Green-blue color */
         font-family: "Trebuchet MS", sans-serif;
-        text-align: left;                 /* Left alignment (optional) */
+        text-align: left; /* Left alignment */
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
+
 st.markdown('<h1 class="custom-title">Fertilizers Dashboard</h1>', unsafe_allow_html=True)
+
 
