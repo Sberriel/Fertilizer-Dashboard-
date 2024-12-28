64c35edd4435c762e68b9fae0a1835d655d9ccc0
Sberriel <sba24281@student.cct.ie>
2024-12-28 11:03:32 +0000

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,6 +1,21 @@
 import streamlit as st 
 import pandas as pd
 
-st.write ("Fertilizers Dashboard")
 
 df = pd.read_csv("FAOSTAT_11.csv")
+
+st.write ("Fertilizers Dashboard")
+
+st.markdown(
+    """
+    <style>
+    .custom-title {
+        font-size: 120px;       
+        color: ##0FA3B1;        
+        font-family: Trebichet MS, sans-serif; 
+        text-align: left;     
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
