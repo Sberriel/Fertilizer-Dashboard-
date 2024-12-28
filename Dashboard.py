8d66db11db53b0d3b4b29276f46a4466a1e9e4a1
Sberriel <sba24281@student.cct.ie>
2024-12-28 11:17:53 +0000

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -3,20 +3,8 @@ import pandas as pd
 
 df = pd.read_csv('FAOSTAT.csv')
 
-st.markdown("Fertilizers ashboard")
+st.write("Fertilizers ashboard")
 
 
-st.markdown(
-    """
-    <style>
-    .custom-title {
-        font-size: 85px;             /* Large font size */
-        color: #0FA3B1;              /* Greenish-blue color */
-        font-family: "Trebuchet MS", sans-serif;  /* Trebuchet MS font */
-        text-align: left;            /* Left-aligned text */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
 
