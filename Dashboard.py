63be84d907c2921c87c6f612a1f96dd581a99ada
Sberriel <sba24281@student.cct.ie>
2024-12-28 11:15:31 +0000

    a
    a

--- Dashboard.py
+++ Dashboard.py
@@ -1,18 +1,19 @@
 import streamlit as st 
 import pandas as pd
 
-df = pd.read_csv('FAOSTAT_11.csv')
+df = pd.read_csv('FAOSTAT.csv')
 
-st.markdown ("Fertilizers Dashboard")
+st.markdown("Fertilizers Dashboard")
+
 
 st.markdown(
     """
     <style>
     .custom-title {
-        font-size: 85px;       
-        color: ##0FA3B1;        
-        font-family: Trebichet MS, sans-serif; 
-        text-align: left;     
+        font-size: 85px;             /* Large font size */
+        color: #0FA3B1;              /* Greenish-blue color */
+        font-family: "Trebuchet MS", sans-serif;  /* Trebuchet MS font */
+        text-align: left;            /* Left-aligned text */
     }
     </style>
     """,
