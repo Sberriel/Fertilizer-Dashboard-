607ec74bf82d4eb4f3f5e3b83eb3279af22ade53
Sberriel <sba24281@student.cct.ie>
2024-12-28 11:28:23 +0000

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -3,8 +3,22 @@ import pandas as pd
 
 df = pd.read_csv('FAOSTAT.csv')
 
-st.write("Fertilizers Dashboard")
+ st.title("Fertilizers Dashboard")
 
 
+st.markdown(
+    """
+    <style>
+    .custom-title {
+        font-size: 85px;                  /* Large font size */
+        color: #0FA3B1;                   /* Green-blue color */
+        font-family: "Trebuchet MS", sans-serif;
+        text-align: left;                 /* Left alignment (optional) */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
+st.markdown('<h1 class="custom-title">Fertilizers Dashboard</h1>', unsafe_allow_html=True)
 
